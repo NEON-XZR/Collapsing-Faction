6a095ae68731bd15d708cdf6b540a35b2b7325b8
Neon <78184988+NEON-XZR@users.noreply.github.com>
2025-03-13 14:38:30 +0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+let mod = Vars.mods.locateMod("colfac");
+mod.meta.displayName = Core.bundle.get("mod.colfac.name");
+mod.meta.subtitle = Core.bundle.get("mod.colfac.subtitle");
+mod.meta.description = Core.bundle.get("mod.colfac.description");
+// special thanks to meep for the help!!
